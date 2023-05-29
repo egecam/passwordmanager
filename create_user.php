28b4ccbc73c0e5087f7008ab2abd1747bd89d473
Ege <egecam000@gmail.com>
2023-05-30 00:25:06 +0300

    redirection changed

--- create_user.php
+++ create_user.php
@@ -4,28 +4,29 @@ if (isset($_POST['submit'])) {
     $username = "root";
     $password = "";
     $dbname = "passwordmanager";
-    
+
     $conn = new mysqli($servername, $username, $password, $dbname);
-    
+
     // Check the connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    
+
     $newUsername = $_POST['new_username'];
     $newPassword = $_POST['new_password'];
 
     function encrypt($data)
-{
-    // Perform encryption logic here (e.g., using a secure encryption algorithm)
-    // ...
-    $encryptionKey =  "7075166676";
-    $encryptedData = openssl_encrypt($data, 'AES-256-CBC', $encryptionKey, 0, substr(hash('sha256', $encryptionKey), 0, 16));
-    return $encryptedData;
-}
+    {
+        // Perform encryption logic here (e.g., using a secure encryption algorithm)
+        // ...
+        $encryptionKey = "7075166676";
+        $encryptedData = openssl_encrypt($data, 'AES-256-CBC', $encryptionKey, 0, substr(hash('sha256', $encryptionKey), 0, 16));
+        return $encryptedData;
+    }
 
 
-    function userExists($username, $conn) {
+    function userExists($username, $conn)
+    {
         $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
         $stmt->bind_param("s", $username);
         $stmt->execute();
@@ -40,20 +41,20 @@ if (isset($_POST['submit'])) {
 
     if (userExists($newUsername, $conn)) {
         // User with the provided username or email already exists
-        echo "Username already exists.";
+        echo "Username already exists. Redirecting to Login Page in 3 seconds...";
     } else { // Insert the new user into the database   
         $query = "INSERT INTO users (username, password) VALUES ('$newUsername', '$newPassword')";
-        
+
         if ($conn->query($query) === TRUE) {
-            echo "New user created successfully! Will redirect in 5 seconds...";
+            echo "New user created successfully! Will redirect in 3 seconds...";
         } else {
             echo "Error: " . $query . "<br>" . $conn->error;
         }
     }
-    
+
     // Close the database connection
     $conn->close();
 }
 
-header('refresh: 5; url=login_screen.php');
+header('refresh: 3; url=login_screen.php');
 ?>
