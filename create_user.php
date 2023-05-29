719ca2c22b44c03458562e7b69074c77ec02200c
yigitozman <108322277+yigitozman@users.noreply.github.com>
2023-05-29 17:39:01 +0300

    Added fix for creating duplicate accounts

--- create_user.php
+++ create_user.php
@@ -14,14 +14,28 @@ if (isset($_POST['submit'])) {
     
     $newUsername = $_POST['new_username'];
     $newPassword = $_POST['new_password'];
-    
-    // Insert the new user into the database
-    $query = "INSERT INTO users (username, password) VALUES ('$newUsername', '$newPassword')";
-    
-    if ($conn->query($query) === TRUE) {
-        echo "New user created successfully! Will redirect in 5 seconds...";
-    } else {
-        echo "Error: " . $query . "<br>" . $conn->error;
+
+    function userExists($username, $conn) {
+        $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $stmt->store_result();
+        $num_rows = $stmt->num_rows;
+        $stmt->close();
+        return $num_rows > 0;
+    }
+
+    if (userExists($newUsername, $conn)) {
+        // User with the provided username or email already exists
+        echo "Username already exists.";
+    } else { // Insert the new user into the database
+        $query = "INSERT INTO users (username, password) VALUES ('$newUsername', '$newPassword')";
+        
+        if ($conn->query($query) === TRUE) {
+            echo "New user created successfully! Will redirect in 5 seconds...";
+        } else {
+            echo "Error: " . $query . "<br>" . $conn->error;
+        }
     }
     
     // Close the database connection
